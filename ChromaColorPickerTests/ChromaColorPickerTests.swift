5a6c2fdd7cc52bcbea0ad6a6b400a700af08a3bd
lucianotoools <luciano@toools.es>
2019-04-02 12:46:40 +0200

    swift5 + swiftlint

--- ChromaColorPickerTests/ChromaColorPickerTests.swift
+++ ChromaColorPickerTests/ChromaColorPickerTests.swift
@@ -8,47 +8,52 @@
 
 import EarlGrey
 import XCTest
+import ChromaColorPicker
 @testable import ChromaColorPicker_Demo
 
-let EarlGrey = EarlGreyImpl.invoked(fromFile: #file, lineNumber: #line)!
+let earlGrey = EarlGreyImpl.invoked(fromFile: #file, lineNumber: #line)!
 
 class ChromaColorPickerTests: XCTestCase {
-    
+
     var viewController: ViewController!
     var colorPicker: ChromaColorPicker!
-    
+
     override func setUp() {
+
         super.setUp()
-        
+
         let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
-        viewController = storyboard.instantiateInitialViewController() as! ViewController
+        if let controller = storyboard.instantiateInitialViewController() as? ViewController {
+
+            viewController = controller
+        }
         UIApplication.shared.keyWindow!.rootViewController = viewController
-        
+
         XCTAssertNotNil(viewController.colorPicker)
         viewController.colorPicker.isAccessibilityElement = true //set for earlgrey
         viewController.colorPicker.accessibilityLabel = "color_picker"
         colorPicker = viewController.colorPicker
     }
-    
+
     func testAdjustToColor() {
+
         let testColor = UIColor(red: 155/255.0, green: 107/255.0, blue: 255/255.0, alpha: 1)
         colorPicker.adjustToColor(testColor)
         let pickerColor = colorPicker.currentColor
-        
+
         XCTAssertEqual(pickerColor.description, testColor.description, "Picker did not properly adjust to color") //Compare description because of fractional differences
-        
+
         XCTAssertEqual(colorPicker.hexLabel.text, "#\(testColor.hexCode)", "Picker displays incorrect Hex Value")
     }
-    
+
     func testButtonDelegateRecieved() {
-        let pickerMatch = grey_allOfMatchers([ grey_accessibilityLabel("color_picker") ])
-            
+
+        let pickerMatch = grey_allOfMatchers([ grey_accessibilityLabel("color_picker")! ])
+
         let colorOnTap = colorPicker.currentColor
-        EarlGrey.selectElement(with: pickerMatch)
-                .perform(grey_tap())
-            
+        earlGrey.selectElement(with: pickerMatch)
+            .perform(grey_tap())
+
         XCTAssertTrue(viewController.colorDisplayView.backgroundColor!.description == colorOnTap.description)
     }
-    
-    
 }
